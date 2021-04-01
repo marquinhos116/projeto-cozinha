6b50e12d22f0c01d58bd54b2caa8a9c9e8ce5387
Marcos Paulo <marquinhos.jesus44@gmail.com>
2021-03-31 22:06:51 -0300

    Modificações de leitura

--- Restaurante/Program.cs
+++ Restaurante/Program.cs
@@ -11,25 +11,25 @@ namespace Restaurante
         {
             List<Cozinha> cozinha = new List<Cozinha>();
             int n = 0;
-            Console.Write("Informe quantos restaurantes ira Cadastrar: ");
+            Console.Write("Qual a quantidade de Restaurantes irá cadastrar? ");
             int numeroRestaurantes = int.Parse(Console.ReadLine());
 
             while (n < numeroRestaurantes)
             {
                 Console.WriteLine("\n\nCadastrando Restaurante\n");
-                Console.Write("Informe o tipo de cozinha: ");
+                Console.Write("Coloque o tipo de cozinha: ");
                 string tipoCozinha = Console.ReadLine();
-                Console.Write("Infome a quantidade de funcionarios: ");
+                Console.Write("Qual a quantidade de funcionarios: ");
                 int numeroFuncioarios = int.Parse(Console.ReadLine());
-                Console.Write("Infome a quantidade de ingredientes:  ");
+                Console.Write("Qual a quantidade de ingredientes:  ");
                 int numeroIngredientes = int.Parse(Console.ReadLine());
-                Console.Write("Infome o tempo de espera: ");
+                Console.Write("Qual o tempo de espera: ");
                 int tempoEspera = int.Parse(Console.ReadLine());
-                Console.Write("Infome a hora de abertura ");
+                Console.Write("Qual a hora de abertura ");
                 int horaAbertura = int.Parse(Console.ReadLine());
-                Console.Write("Infome a hora de fechamento: ");
+                Console.Write("Qual a hora de fechamento: ");
                 int horaFechamento = int.Parse(Console.ReadLine());
-                Console.Write("Infome o prato principal: ");
+                Console.Write("Qual o prato principal: ");
                 string pratoPrincipal = Console.ReadLine();
                 cozinha.Add(new Cozinha(
                     numeroIngredientes,
@@ -40,21 +40,21 @@ namespace Restaurante
                     tipoCozinha,
                     pratoPrincipal
                     ));
-                Console.WriteLine("\nCadastrando ingredientes\n");
+                Console.WriteLine("\nCadastro De ingredientes\n");
                 for (int i = 0; i < cozinha[n].numeroIngredientes(); i++)
                 {
-                    Console.Write("Entre com o nome do ingrediente: ");
+                    Console.Write("Descreva o nome do ingrediente: ");
                     string nome = Console.ReadLine();
-                    Console.Write("Entre com a data de Vencimento: ");
+                    Console.Write("Qual a data de Vencimento: ");
                     string data = Console.ReadLine();
                     cozinha[n].AdicionarIngredientes(nome, data);
                 }
                 Console.WriteLine("\nCadastrando Funcionarios\n");
                 for (int i = 0; i < cozinha[n].NumeroFuncionarios(); i++)
                 {
-                    Console.Write("Infome o nome do funcionario: ");
+                    Console.Write("Descreva o nome do funcionario: ");
                     string nome = Console.ReadLine();
-                    Console.Write("Infome a atividade do funcionario: ");
+                    Console.Write("Qual a atividade do funcionario: ");
                     string atividade = Console.ReadLine();
                     cozinha[n].AdicionarFuncionarios(nome, atividade);
                 }
@@ -63,20 +63,20 @@ namespace Restaurante
             Console.Clear();
             foreach (var item in cozinha)
             {
-                Console.WriteLine($"<><><><><><><><><><><><> Cozinha <><><><><><><><><><><>\n");
+                Console.WriteLine($"--------------------Cozinha-----------------------------\n");
                 Console.WriteLine(item);
-                Console.WriteLine("\n<><><><><><><><><><><> Ingredientes <><><><><><><><><><>\n");
+                Console.WriteLine("\n--------------------> Ingredientes---------------------\n");
                 for (int i = 0; i < item.numeroIngredientes(); i++)
                 {
                     Console.WriteLine(item.retornaIngrediente(i));
                 }
-                Console.WriteLine("\n<><><><><><><><><><><> Funcionario  <><><><><><><><><><>\n");
+                Console.WriteLine("\n---------------------> Funcionario---------------------\n");
                 for (int i = 0; i < item.NumeroFuncionarios(); i++)
                 {
                     Console.WriteLine(item.RetornaFuncionarios(i));
                 }
-                Console.WriteLine("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
-                Console.WriteLine("////////////////////////////////////////////////////////");
+                Console.WriteLine("\n ----------------C'EST FINIT Á BIENTOT----------------");
+                Console.WriteLine("--------------------------------------------------------");
                 Console.WriteLine("\n");
             }
 
